ad541debdcc7d312731d076b03e77ae41ca8fa65
Chemseddine <benmahamedchemseddine@gmail.com>
2023-06-04 23:26:31 +0100

    first commit

--- former1.php
+++ former1.php
@@ -21,8 +21,33 @@ $date_routeur  = $_POST['date_routeur'];
 
 
 
+// Récupérer la clé client à partir du pseudo client
+$sql = "SELECT ID_C FROM client WHERE Pseudo_C = '$Pseudo_C'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $ID_Client = $row['ID_C']; // Récupérer ID du client
+} else {
+    echo "Client non trouvé.";
+    exit; // Arrêter l'exécution si non client
+}
+
+// Partie id produit
+$sql = "SELECT ID_P FROM article WHERE Libelle = '$Libelle'";
+$result = $conn->query($sql);
+
+if ($result->num_rows > 0) {
+    $row = $result->fetch_assoc();
+    $ID_Produit = $row['ID_P']; 
+} else {
+    echo "Produit non trouvé.";
+    exit; 
+}
+
+
 // Insertion des info dans la BDD
-$sql = "INSERT INTO location (Libelle, Pseudo_C, N_telephone, N_didentite, Date_Debut, Date_Fin) VALUES ('$Libelle', '$Pseudo_C', '$N_telephone', '$N_didentite', '$date_debut', '$date_routeur')";
+$sql = "INSERT INTO location (Libelle, Pseudo_C, N_telephone, N_didentite, Date_Debut, Date_Fin, ID_C, ID_P) VALUES ('$Libelle', '$Pseudo_C', '$N_telephone', '$N_didentite', '$date_debut', '$date_routeur', '$ID_Client', '$ID_Produit')";
     
 if ($conn->query($sql) === TRUE) {
     header('Location: Fournisseur.php');
